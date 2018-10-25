532705d115e0116e406cdff47962465c9d5e1b0f
Ji-Ho Sung <slayer1000@Ji-Ho-Sung-ui-MacBook-Air.local>
2018-10-25 21:00:51 +0900

    ch 14 cargo

---
+++ rust/ch14/my_crate/src/lib.rs
@@ -0,0 +1,23 @@
+//! # My Carte
+//! 
+//! 'my carte' is a collection of utilites to make perfoming certain
+//! calcurations more convenient.
+
+/// Add one to the number givens
+/// 
+/// #Example
+/// 
+/// ```
+/// let five = 5;
+/// assert_eq!(6, my_crate::add_one(five));
+/// ```
+/// 
+/// #Panics
+/// naver panics
+/// 
+/// #Safety
+/// allways safety
+/// 
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
